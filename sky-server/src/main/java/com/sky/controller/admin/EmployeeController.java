aee54619a5b74d7e96acf8d59a1aac0bea8e284a
潘帅康 <1991290053@qq.com>
2026-08-24 18:35:10 +0800

    增加了新增员工业务

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -70,5 +71,17 @@ public class EmployeeController {
     public Result<String> logout() {
         return Result.success();
     }
+
+
+    //新增员工
+    @PostMapping
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        //调用service
+        employeeService.save(employeeDTO);
+        //返回结果
+        return Result.success();
+    }
+
+
 
 }
