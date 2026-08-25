cd30551c4b1e33306fd2176ddd19c38a74a4664c
潘帅康 <1991290053@qq.com>
2026-08-25 21:29:45 +0800

    增加了员工分页查询业务

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -24,7 +26,6 @@ public interface EmployeeMapper {
     void insert(Employee employee);
 
 
-
-
-
+    //员工信息分页查询
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
